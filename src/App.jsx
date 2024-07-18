d24d281074cb1c92f689365b2adf03dacd410899
devesh tripathi <deveshtrip10@gmail.com>
2024-07-18 22:00:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Login from './components/Login'
+import Register from './components/Register'
+import Recipe from './components/Recipe'
+
+
+const App = () => {
+ 
+  return (
+   <>
+   <Register/>
+   {/* <Login/> */}
+   <Recipe/>
+   </>
+  )
+}
+
+export default App
